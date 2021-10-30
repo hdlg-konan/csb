f730ea5eef0e7c57ac64dffdce6d4a643bd6e4a6
hdlg-konan <konan.le.destructeur@gmail.com>
2021-10-30 12:05:50 +0000

    Change

--- csb/csb/doctype/csb_settings/csb_settings.js
+++ csb/csb/doctype/csb_settings/csb_settings.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, XLevel Retail Systems Nigeria Ltd and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Paystack Settings', {
+frappe.ui.form.on('CSB Settings', {
 	refresh: function(frm) {
 
 	}
