9f6cde0ef01dd0830c7cfba8f2d465b531def36a
hdlg-konan <konan.le.destructeur@gmail.com>
2021-10-31 20:20:16 +0000

    Change

--- csb/csb/doctype/csb_settings/csb_settings.py
+++ csb/csb/doctype/csb_settings/csb_settings.py
@@ -36,7 +36,8 @@ class CSBSettings(Document):
 	def validate_credentials(self):
 		try:
 			secret = self.get_password(fieldname='secret_key', raise_exception=False)
-			headers = {'Authorization': 'Basic '}
+			base64string = base64.encodebytes(('%s:%s' % (public_key, secret)).encode('utf8')).decode('utf8').replace('\n', '')
+			headers = ("Authorization: Basic %s" % base64string)
 			api_URL = "https://epaync.nc/api-payment/V4/Charge/SDKTest"
 			response = requests.request("GET",api_url, headers=headers)
 		except ConnectionError:
