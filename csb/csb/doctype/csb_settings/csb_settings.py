5ff38d687db68861cf0066c256cfa40a3a34b11f
hdlg-konan <konan.le.destructeur@gmail.com>
2021-11-01 01:45:22 +0000

    Change

--- csb/csb/doctype/csb_settings/csb_settings.py
+++ csb/csb/doctype/csb_settings/csb_settings.py
@@ -67,7 +67,7 @@ class CSBSettings(Document):
 
 		base64string = base64.encodebytes(('%s:%s' % (self.public_key, self.secret_key)).encode('utf8')).decode('utf8').replace('\n', '')
 		api_url = "https://epaync.nc/api-payment/V4/Charge/CreatePayment"
-		headers = ("Authorization: Basic %s" % base64string)  
+		headers = {"Authorization: Basic %s" % base64string}  
 
 		payment_options = {
 			"amount": kwargs.get('amount'),
